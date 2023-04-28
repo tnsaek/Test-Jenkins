7691bc79a9b048919efc773d26f286d15dec1c80
tnsaek <120763600+tnsaek@users.noreply.github.com>
2023-04-28 18:27:02 -0500

    Update TestJenkinsApplication.java
    test for auto jenkins build

--- src/main/java/testjenkins/TestJenkinsApplication.java
+++ src/main/java/testjenkins/TestJenkinsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestJenkinsApplication {
-
+        // test for auto jenkins build
 	public static void main(String[] args) {
 		SpringApplication.run(TestJenkinsApplication.class, args);
 	}
